b88ddbd8b8d55d7c988cafefe5505b3bea48cc86
Daniel Adu <danieladu.dtc@outlook.com>
2019-01-10 10:09:44 +0000

    changes made to divide,multiply html values and equal-to operations applied

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -3,5 +3,14 @@ function insert(digit) {
 }
 
 function erase() {
-	document.display.text_holder.value="";
+	document.display.text_holder.value = "";
 }
+
+function calc() {
+	var getData = document.display.text_holder.value;
+
+	if(getData) {
+		document.display.text_holder.value = eval(getData);
+	}
+}
+
